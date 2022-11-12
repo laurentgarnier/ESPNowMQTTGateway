79351fd47326ce62d81d2e21d24f9f77b583e9ed
Laurent Garnier <laurentgarnier@users.noreply.github.com>
2022-11-12 11:32:27 +0100

    Update credentials.h
    Anonymisation

--- WifiGateway/include/credentials.h
+++ WifiGateway/include/credentials.h
@@ -1,5 +1,5 @@
-const char*     ssid              = "CG";
-const char*     password          = "12ImpEglAnt69";
-const char*     mqtt_server       = "192.168.10.200";
+const char*     ssid              = "[SSID]";
+const char*     password          = "[SSID_Password]";
+const char*     mqtt_server       = "[MQTT_Server_IP]";
 const char*     mqtt_username     = "";
 const char*     mqtt_password     = "";
